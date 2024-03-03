0d91c99e7373b561fb807c0653df6edba0675062
sayu1803 <sayantanrabi.03@gmail.com>
2024-03-03 19:54:05 +0530

    3rd march

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3306
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login', (req,res)=>{
+    res.send('please login at chai aur code')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
